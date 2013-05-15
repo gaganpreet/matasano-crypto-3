4a41c8a198a62877f2bb14869eef8a44256bc265
Gaganpreet <gaganpreet.arora@gmail.com>
2013-05-15 21:33:57 +0530

    Added progress bar to MT19337 seed cracker

--- src/crack_mt_seed.py
+++ src/crack_mt_seed.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import random
 from mersenne_twister import MersenneTwister
 
@@ -8,15 +9,28 @@ def rng():
     time.sleep(random.randint(42, 1024))
     return mt.next()
 
+def update_progress(done, total):
+    sys.stdout.write('\b'*4)
+    progress = str(int(100.0*done/total))
+    text = (3 - len(progress)) * ' ' + progress +  '%'
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
 def crack_seed(r):
     now = int(time.time())
-    start = int(now - 5000)
-    end = int(now + 5000)
+    start = (now - 2000)
+    end = now
 
-    for i in xrange(start, end):
+    for i in xrange(start, end+1):
+        update_progress(i - start, end-start)
         if MersenneTwister(i).next() == r:
+            print
             return i
+    print
+    return "FAILED"
 
 if __name__ == '__main__':
+    print 'Sleeping now, will take a while!'
     r = rng()
+    print 'Starting to crack now!'
     print crack_seed(r)
